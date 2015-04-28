89e209509567b9e0694fefbfe8221e7be1d5da6b
Kaguya <1136358656@qq.com>
2015-04-28 15:00:47 +0800

    up

--- shadowsocks-csharp/View/FormLogin.Designer.cs
+++ shadowsocks-csharp/View/FormLogin.Designer.cs
@@ -91,6 +91,7 @@
             // 
             // button2
             // 
+            this.button2.Enabled = false;
             this.button2.Location = new System.Drawing.Point(32, 177);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(90, 29);
